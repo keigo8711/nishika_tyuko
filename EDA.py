4aa501d376546e48a24f665c68de1e0e2161a0bf
u-keigo8711 <u-keigo@outlook.jp>
2023-11-11 16:55:58 +0900

    Not install statsmodels for VIF

--- EDA.py
+++ EDA.py
@@ -2,22 +2,53 @@
     # Combine files, Comvert category variables, Compensate null
 ####################################################################
 
+import os
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from MyPackege import cleaning
+# from statsmodels.stats.outliers_influence import variance_inflation_factor
+
 
 
 if __name__ == '__main__':
     path_to_get = '/Users/keigo/github/nishika/tyuko_mansyon/data/train'
     path_to_save = '/Users/keigo/github/nishika/tyuko_mansyon/data'
-    label_encoded_columns = ['都道府県名', '市区町村名', '地区名', '最寄駅：名称', '間取り', '都市計画', '改装', '取引の事情等']
+    path_for_result = '/Users/keigo/github/nishika/tyuko_mansyon/result'
+    label_encoded_columns = ['種類', '都道府県名', '市区町村名', '地区名', '最寄駅：名称', '間取り', '建物の構造', '都市計画', '改装', '取引の事情等']
+    standardized_columns = ['最寄駅：距離（分）', '面積（㎡）', '建築年', '建ぺい率（％）', '容積率（％）']
 
     Preprocessor = cleaning.Preprocessor(before_path=path_to_get, after_path=path_to_save)
+
+    # In case we need reprocess data
+    Preprocessor.transform_moyori()
+    Preprocessor.transform_kenchiku()
+    Preprocessor.transform_torihiki()
+    Preprocessor.fill_null_basic()
     Preprocessor.encode_label(columns=label_encoded_columns)
-
     arranged_df = Preprocessor.return_df()
-    # Preprocessor.save_df()
-    # arranged_df = Preprocessor.recall_df()
+    print(arranged_df.isnull().sum())
 
-    df = arranged_df.head(10)
-    print(df)
+    # Call data & confirm number of null
+    Preprocessor.fill_null_advance(method='median', target_col=['最寄駅：距離（分）', '建ぺい率（％）', '容積率（％）'], referred_col=['面積（㎡）', '建築年'])
+    Preprocessor.english_columns()
+    arranged_df = Preprocessor.return_df()
+    print(arranged_df.isnull().sum())
+    Preprocessor.save_df()
+
+    # Visualize and save correlation matrix
+    df = arranged_df.astype('float64', errors='ignore')
+    df = df.select_dtypes(include='float64')
+    colormap = plt.cm.RdBu
+    plt.figure(figsize=(14,14))
+    plt.title('Pearson Correlation of Features', y=1.05, size=15)
+    sns.heatmap(df.corr(), linewidths=0.1, vmax=1.0, square=True,
+                cmap=colormap, linecolor='white', annot=True)
+    plt.savefig(os.path.join(path_for_result, 'EDA_result1.png'))
+
+    # # Calculate VIF
+    # vif = pd.DataFrame()
+    # vif["VIF Factor"] = [variance_inflation_factor(df.values, i) for i in range(df.shape[1])]
+    # vif["features"] = df.columns
+    # print(vif)
--- MyPackege/cleaning.py
+++ MyPackege/cleaning.py
@@ -5,7 +5,17 @@
 import os
 import glob
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+import numpy as np
+import re
+from sklearn.preprocessing import LabelEncoder
+
+
+def wareki2seireki(wareki_s, def_value=0):
+    era_dic = {"明治": 1868, "大正": 1912, "昭和": 1926, "平成": 1989, "令和": 2019}
+    s = re.match(r'(明治|大正|昭和|平成|令和)([0-9]+|元)年', str(wareki_s))
+    if s is None: return def_value
+    y = int(s.group(2)) if s.group(2) != '元' else 1
+    return era_dic[s.group(1)] + y - 1
 
 
 class Preprocessor:
@@ -23,23 +33,113 @@ class Preprocessor:
                 self.df = pd.read_csv(file_path)  # Initialize
             else:
                 self.df = pd.concat([self.df, pd.read_csv(file_path)])  # Append
+        self.df.replace("", np.nan, inplace=True)
+        self.df = self.df.reset_index(drop=True)
 
     def return_df(self) -> pd.DataFrame:
         print('-- End pre-processing --')
         return self.df
-    
+
     def save_df(self):
         path = self.return_path + '/saved_data.csv'
         self.df.to_csv(path)
         print('-- End pre-processing --')
-    
+
     def recall_df(self) -> pd.DataFrame:
         path = self.return_path + '/saved_data.csv'
-        return pd.read_csv(path)
-    
+        self.df = pd.read_csv(path)
+        return self.df
+
     def encode_label(self, columns: list):
         # Label encoding
         for ii in range(len(columns)):
             col = columns[ii]
             sex_le = LabelEncoder()
             self.df[col] = sex_le.fit_transform(self.df[col])
+
+    def transform_moyori(self):
+        # apply transformation for each
+        for ii in self.df.index:
+            if ii == int(len(self.df.index)/2):
+                print('- Half time -')
+
+            if self.df.loc[ii,'最寄駅：距離（分）'] == '30分?60分':
+                self.df.loc[ii,'最寄駅：距離（分）'] = 45
+            elif self.df.loc[ii,'最寄駅：距離（分）'] == '1H?1H30':
+                self.df.loc[ii,'最寄駅：距離（分）'] = 75
+            elif self.df.loc[ii,'最寄駅：距離（分）'] == '1H30?2H':
+                self.df.loc[ii,'最寄駅：距離（分）'] = 105
+            elif self.df.loc[ii,'最寄駅：距離（分）'] == '2H?':
+                self.df.loc[ii,'最寄駅：距離（分）'] = 120
+
+            # if self.df.loc[ii,'最寄駅：距離（分）'] != np.nan:
+            #     self.df.loc[ii,'最寄駅：距離（分）'] = int(self.df.loc[ii,'最寄駅：距離（分）'])
+
+    def transform_kenchiku(self):
+        # apply transformation for each
+        for ii in self.df.index:
+            if ii == int(len(self.df.index)/2):
+                print('- Half time -')
+
+            if self.df.loc[ii,'建築年'] == '戦前':
+                self.df.loc[ii,'建築年'] = 1945
+            elif self.df.loc[ii,'建築年'] != np.nan:
+                self.df.loc[ii,'建築年'] = wareki2seireki(self.df.loc[ii,'建築年'])
+
+    def transform_torihiki(self):
+        # apply transformation for each
+        self.df['取引年'] = np.nan
+        self.df['取引四半期'] = np.nan
+        for ii in self.df.index:
+            if ii == int(len(self.df.index)/2):
+                print('- Half time -')
+
+            if self.df.loc[ii,'取引時点'] != np.nan:
+                string = self.df.loc[ii,'取引時点']
+                self.df['取引年'] = int(string[:4])
+                self.df['取引四半期'] = int(string[6])
+
+    def fill_null_basic(self):
+        self.df.fillna(self.df.median(numeric_only=True))
+
+    def fill_null_advance(self, method: str, target_col: list, referred_col: list):
+        method_to_fill = ['median', 'mean']
+        for ii in range(len(target_col)):
+            if method == method_to_fill[0]:
+                print('Null is filled by ' + str(method_to_fill[0]))
+                self.df[target_col[ii]]= self.df.groupby(referred_col)[target_col[ii]].apply(lambda row : row.fillna(row.median()))
+            elif method == method_to_fill[1]:
+                print('Null is filled by ' + str(method_to_fill[1]))
+                self.df[target_col[ii]]= self.df.groupby(referred_col)[target_col[ii]].apply(lambda row : row.fillna(row.mean()))
+            else:
+                raise ValueError("Select your method in " + str(method_to_fill))
+
+    def english_columns(self):
+        column_mapping = {'種類': 'Type',
+                          '地域': 'Region',
+                          '市区町村コード': 'Municipalities code',
+                          '都道府県名': 'Prefecture name',
+                          '市区町村名': 'Municipalities name',
+                          '地区名': 'District name',
+                          '最寄駅：名称': 'Nearest station name',
+                          '最寄駅：距離（分）': 'Nearest station minutes',
+                          '間取り' : 'Floor plan',
+                          '面積（㎡）': 'Area',
+                          '土地の形状': 'Shape of land',
+                          '間口': 'Frontage',
+                          '延床面積（㎡）': 'Total floor area',
+                          '建築年': 'Build year',
+                          '建物の構造': 'Structure of building',
+                          '用途': 'Use case',
+                          '今後の利用目的': 'Future purpose',
+                          '前面道路：方位': 'Road direction',
+                          '前面道路：種類': 'Road type',
+                          '前面道路：幅員（ｍ）': 'Road width',
+                          '都市計画': 'Urban planning',
+                          '建ぺい率（％）': 'Building coverage ratio',
+                          '容積率（％）': 'Floor-area ratio',
+                          '取引時点': 'Transaction point',
+                          '改装': 'Renewal',
+                          '取引の事情等': 'Memo',
+                          '取引価格（総額）_log': 'Sales value (log)'}
+        self.df.rename(columns=column_mapping, inplace=True)
